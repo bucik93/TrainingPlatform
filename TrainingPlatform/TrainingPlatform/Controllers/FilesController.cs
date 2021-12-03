bceda459c24c7ccabdbef6a3260507011b80a1a7
bucik93 <piotrbucior93@gmail.com>
2021-12-03 11:53:51 +0100

    Add Files Controller

---
+++ TrainingPlatform/TrainingPlatform/Controllers/FilesController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TrainingPlatform.ApplicationServices.API.Domain;
+using TrainingPlatform.DataAccess;
+using TrainingPlatform.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingPlatform.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public FilesController(IMediator mediator)
+        {
+            this.mediator = mediator;
+
+        }
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllFiles([FromQuery] GetContentFilesRequest request)
+        {
+            var response = await this.mediator.Send(request);
+            return this.Ok(response);
+        }
+    }
+}
