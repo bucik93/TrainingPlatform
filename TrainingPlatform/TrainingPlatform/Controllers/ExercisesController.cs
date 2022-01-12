7ada09821860a5bc12e6c962660c28433c871506
bucik93 <piotrbucior93@gmail.com>
2022-01-12 13:40:56 +0100

    Adding NLog logger.

--- TrainingPlatform/TrainingPlatform/Controllers/ExercisesController.cs
+++ TrainingPlatform/TrainingPlatform/Controllers/ExercisesController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace TrainingPlatform.Controllers
 {
@@ -15,15 +16,19 @@ namespace TrainingPlatform.Controllers
     public class ExercisesController : ApiControllerBase
     {
         private readonly IMediator mediator;
+        private readonly ILogger<ExercisesController> logger;
 
-        public ExercisesController(IMediator mediator) : base(mediator)
+        public ExercisesController(IMediator mediator, ILogger<ExercisesController> logger) : base(mediator)
         {
+            this.logger = logger;
+            logger.LogInformation("We are in ExerciseController.");
         }
 
         [HttpGet]
         [Route("")]
         public Task<IActionResult> GetAllExercises([FromQuery] GetExercisesRequest request)
         {
+            logger.LogInformation("Get All Exercises.");
             return this.HandleRequest<GetExercisesRequest, GetExercisesResponse>(request);
         }
 
@@ -31,6 +36,7 @@ namespace TrainingPlatform.Controllers
         [Route("")]
         public Task<IActionResult> AddExercise([FromQuery] AddExerciseRequest request)
         {
+            logger.LogInformation($"Add exercise {request.Name} to database.");
             return this.HandleRequest<AddExerciseRequest, AddExerciseResponse>(request);
         }
 
@@ -39,6 +45,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> GetById([FromRoute] int id)
         {
             var request = new GetExerciseByIdRequest() { ExerciseId = id };
+            logger.LogInformation($"Get exercise by Id: {id}.");
             return this.HandleRequest<GetExerciseByIdRequest, GetExerciseByIdResponse>(request);
         }
         [HttpDelete]
@@ -46,6 +53,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> Remove([FromRoute] int id)
         {
             var request = new RemoveExerciseRequest() { ExerciseId = id };
+            logger.LogInformation($"Remove exercise by Id: {id}.");
             return this.HandleRequest<RemoveExerciseRequest, RemoveExerciseResponse>(request);
         }
 
@@ -54,6 +62,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> UpdateExercise([FromRoute] int id, [FromQuery] UpdateExerciseRequest request)
         {
             request.ExerciseId = id;
+            logger.LogInformation($"Update exercise by Id: {id}.");
             return this.HandleRequest<UpdateExerciseRequest, UpdateExerciseResponse>(request);
         }
     }
--- TrainingPlatform/TrainingPlatform/Controllers/PlansController.cs
+++ TrainingPlatform/TrainingPlatform/Controllers/PlansController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace TrainingPlatform.Controllers
 {
@@ -15,9 +16,11 @@ namespace TrainingPlatform.Controllers
     public class PlansController : ApiControllerBase
     {
         private readonly IMediator mediator;
-
-        public PlansController(IMediator mediator) : base(mediator)
+        private readonly ILogger<PlansController> logger;
+        public PlansController(IMediator mediator, ILogger<PlansController> logger) : base(mediator)
         {
+            this.logger = logger;
+            logger.LogInformation("We are in PlanController.");
         }
 
         [HttpGet]
@@ -25,6 +28,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> GetAllPlans()
         {
             var request = new GetPlansRequest();
+            logger.LogInformation("Get All Plans.");
             return this.HandleRequest<GetPlansRequest, GetPlansResponse>(request);
         }
 
@@ -33,6 +37,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> Search([FromRoute] string planName)
         {
             var request = new SearchPlanNameRequest() { Name = planName };
+            logger.LogInformation($"Search plans by PlanName: {planName}.");
             return this.HandleRequest<SearchPlanNameRequest, SearchPlanNameResponse>(request);
         }
 
@@ -41,6 +46,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> GetById([FromRoute] int id)
         {
             var request = new GetPlanByIdRequest() { PlanId = id };
+            logger.LogInformation($"Get plan by Id: {id}.");
             return this.HandleRequest<GetPlanByIdRequest, GetPlanByIdResponse>(request);
         }
 
@@ -49,6 +55,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> Remove([FromRoute] int id)
         {
             var request = new RemovePlanRequest() { PlanId = id };
+            logger.LogInformation($"Remove plan by Id: {id}.");
             return this.HandleRequest<RemovePlanRequest, RemovePlanResponse>(request);
         }
 
@@ -57,6 +64,7 @@ namespace TrainingPlatform.Controllers
         public Task<IActionResult> UpdatePlan([FromRoute] int id, [FromQuery] UpdatePlanRequest request)
         {
             request.PlanId = id;
+            logger.LogInformation($"Update plan by Id: {id}.");
             return this.HandleRequest<UpdatePlanRequest, UpdatePlanResponse>(request);
         }
 
@@ -64,6 +72,7 @@ namespace TrainingPlatform.Controllers
         [Route("")]
         public Task<IActionResult> AddPlan([FromQuery] AddPlanRequest request)
         {
+            logger.LogInformation($"Add plan to database.");
             return this.HandleRequest<AddPlanRequest, AddPlanResponse>(request);
         }
 
@@ -76,6 +85,7 @@ namespace TrainingPlatform.Controllers
                 PlanId = planId,
                 ExerciseId = exerciseId
             };
+            logger.LogInformation($"Add exerciseId: {exerciseId} and planId: {planId} to table ExercisePlans.");
             return this.HandleRequest<AddExerciseToPlanRequest, AddExerciseToPlanResponse>(request);
         }
     }
